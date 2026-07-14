4d9aaac271e789203c2f77b743bfd58786928625
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:27:00 +0530

    Create 1915-CheckIfOneStringSwapCanMakeStringsEqual

---
+++ 1915-CheckIfOneStringSwapCanMakeStringsEqual/1915-CheckIfOneStringSwapCanMakeStringsEqual.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/14/2026, 3:26:59 PM
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        vector<int> diff;
+        
+        for (int i = 0; i < s1.size(); i++) {
+            if (s1[i] != s2[i]) diff.push_back(i);
+        }
+        
+        if (diff.empty()) return true;
+        if (diff.size() != 2) return false;
+        
+        return s1[diff[0]] == s2[diff[1]] && s1[diff[1]] == s2[diff[0]];
+    }
+};
