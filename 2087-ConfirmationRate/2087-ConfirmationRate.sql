02b5f9c88a3dab13a289cef50eb0effcb927d4fd
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:23:22 +0530

    Create 2087-ConfirmationRate

---
+++ 2087-ConfirmationRate/2087-ConfirmationRate.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/14/2026, 3:23:21 PM
+# Write your MySQL query statement below
+
+select s.user_id, round(sum(case when c.action="confirmed" then 1 else 0 end) / count(*),2) as confirmation_rate
+from Signups as s
+left join
+Confirmations as c
+on s.user_id = c.user_id        
+group by s.user_id  
+order by confirmation_rate      
