0282b19dbfadcb37dfaed183474ab87ecbfd0eb8
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:22:48 +0530

    Create 2108-MinimizeTheDifferenceBetweenTargetAndChosenElements

---
+++ 2108-MinimizeTheDifferenceBetweenTargetAndChosenElements/2108-MinimizeTheDifferenceBetweenTargetAndChosenElements.py
@@ -0,0 +1,14 @@
+# Last updated: 7/14/2026, 3:22:48 PM
+class Solution:
+    def minimizeTheDifference(self, mat: List[List[int]], target: int) -> int:
+        R, C = len(mat), len(mat[0])
+        
+        S = set(mat[0])
+        for r in range(1, R):
+            NS = set()
+            for x in set(mat[r]):
+                for y in S:
+                    NS.add(x + y)
+            S = NS
+        
+        return min(abs(x - target) for x in S)
