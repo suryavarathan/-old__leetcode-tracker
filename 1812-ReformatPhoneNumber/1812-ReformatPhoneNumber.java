1d0d7cdcc73e290e544fb0f3a440477692c20863
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:29:07 +0530

    Create 1812-ReformatPhoneNumber

---
+++ 1812-ReformatPhoneNumber/1812-ReformatPhoneNumber.java
@@ -0,0 +1,17 @@
+// Last updated: 7/14/2026, 3:29:07 PM
+class Solution {
+    String modifiedNumber="";
+    public String reformatNumber(String number) {
+        modifiedNumber=number.replace(" ","");
+        modifiedNumber=modifiedNumber.replace("-","");
+        int l=modifiedNumber.length();
+        if(l<=3){
+            return modifiedNumber;
+        } else if(l==4){
+            return modifiedNumber.substring(0,2)+"-"+ modifiedNumber.substring(2,4);
+        } else {
+            modifiedNumber=modifiedNumber.substring(0,3)+"-"+reformatNumber(modifiedNumber.substring(3,l));
+        }
+        return modifiedNumber;
+    }
+}
