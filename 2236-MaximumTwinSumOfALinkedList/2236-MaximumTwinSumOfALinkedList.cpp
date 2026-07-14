52608708d48fdf05c7210f6a45ff2b5ff6c787fe
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:22:11 +0530

    Create 2236-MaximumTwinSumOfALinkedList

---
+++ 2236-MaximumTwinSumOfALinkedList/2236-MaximumTwinSumOfALinkedList.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/14/2026, 3:22:11 PM
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            swap(slow->next, prev);
+            swap(prev, slow);
+        }
+
+        int res = 0;
+        while (slow) {
+            res = max(res, prev->val + slow->val);
+            prev = prev->next;
+            slow = slow->next;
+        }
+
+        return res;
+    }
+};
