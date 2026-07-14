c1c79480646208d2110897662ff2db937653391c
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:21:56 +0530

    Create 2582-MinimumScoreOfAPathBetweenTwoCities

---
+++ 2582-MinimumScoreOfAPathBetweenTwoCities/2582-MinimumScoreOfAPathBetweenTwoCities.py
@@ -0,0 +1,18 @@
+# Last updated: 7/14/2026, 3:21:56 PM
+class Solution:
+    def minScore(self, n: int, roads: list[list[int]]) -> int:
+        root = list(range(n + 1))
+
+        def find(i: int) -> int:
+            root[i] = find(root[i]) if root[i] != i else i
+            return root[i]
+
+        for x, y, _ in roads:
+            root[find(x)] = find(y)
+
+        res, g1 = 10005, find(1)
+        for x, _, d in roads:
+            if find(x) == g1:
+                res = min(res, d)
+
+        return res
