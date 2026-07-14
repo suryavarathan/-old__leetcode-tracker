e99ab1d1be7defa1be5bcef21d6d51726e02fb9b
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:20:51 +0530

    Create 3496-MinimumNumberOfSecondsToMakeMountainHeightZero

---
+++ 3496-MinimumNumberOfSecondsToMakeMountainHeightZero/3496-MinimumNumberOfSecondsToMakeMountainHeightZero.java
@@ -0,0 +1,19 @@
+// Last updated: 7/14/2026, 3:20:51 PM
+class Solution {
+    public long minNumberOfSeconds(int height, int[] times) {
+        long lo = 1, hi = 10000000000000000L;
+
+        while (lo < hi) {
+            long mid = (lo + hi) >> 1;
+            long tot = 0;
+            for (int i = 0; i < times.length && tot < height; i++)
+                tot += (long) (Math.sqrt((double) mid / times[i] * 2 + 0.25) - 0.5);
+            if (tot >= height)
+                hi = mid;
+            else
+                lo = mid + 1;
+        }
+
+        return lo;
+    }
+}
