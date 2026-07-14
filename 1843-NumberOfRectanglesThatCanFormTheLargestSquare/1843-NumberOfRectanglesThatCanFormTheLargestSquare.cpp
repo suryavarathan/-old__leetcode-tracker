1ab86796978651689a04ef0ea944fbfd0362d420
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:28:26 +0530

    Create 1843-NumberOfRectanglesThatCanFormTheLargestSquare

---
+++ 1843-NumberOfRectanglesThatCanFormTheLargestSquare/1843-NumberOfRectanglesThatCanFormTheLargestSquare.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 3:28:25 PM
+class Solution {
+public:
+    int countGoodRectangles(vector<vector<int>>& rectangles) {
+        unordered_map<int,int> maxsquarelen;
+        int maxlen=INT_MIN;
+        for(auto rectangle : rectangles){
+            maxsquarelen[min(rectangle[0],rectangle[1])]++;
+            maxlen = max(maxlen,min(rectangle[0],rectangle[1]));//store the maxpossible length of square formed
+        }
+        return maxsquarelen[maxlen];
+        //return ;
+    }
+};
