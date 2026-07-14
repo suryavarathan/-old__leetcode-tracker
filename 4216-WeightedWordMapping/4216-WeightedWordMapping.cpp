40059e1ba8dc3f6e4db57ff78e2704de04e03671
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:19:21 +0530

    Create 4216-WeightedWordMapping

---
+++ 4216-WeightedWordMapping/4216-WeightedWordMapping.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/14/2026, 3:19:21 PM
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string result;
+        
+        for (const string& word : words) {
+            int sumWeight = 0;
+            
+            for (char ch : word) {
+                sumWeight += weights[ch - 'a'];
+            }
+            
+            int value = sumWeight % 26;
+            
+            result.push_back(char('z' - value));
+        }
+        
+        return result;
+    }
+};
