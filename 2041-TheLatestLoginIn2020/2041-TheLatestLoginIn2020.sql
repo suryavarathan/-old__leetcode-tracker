c7616b928ca9b5ac0ca0485587fda746534d4836
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:24:16 +0530

    Create 2041-TheLatestLoginIn2020

---
+++ 2041-TheLatestLoginIn2020/2041-TheLatestLoginIn2020.sql
@@ -0,0 +1,12 @@
+-- Last updated: 7/14/2026, 3:24:16 PM
+# Write your MySQL query statement below
+SELECT 
+    user_id, 
+    MAX(time_stamp) AS last_stamp
+FROM 
+    Logins
+WHERE 
+    time_stamp >= '2020-01-01'
+    AND time_stamp < '2021-01-01'
+GROUP BY 
+    user_id;
