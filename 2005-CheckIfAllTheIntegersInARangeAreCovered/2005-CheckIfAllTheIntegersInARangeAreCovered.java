42ca24ed1e0d7908baf9e9431064b255c5340ae0
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:25:08 +0530

    Create 2005-CheckIfAllTheIntegersInARangeAreCovered

---
+++ 2005-CheckIfAllTheIntegersInARangeAreCovered/2005-CheckIfAllTheIntegersInARangeAreCovered.java
@@ -0,0 +1,18 @@
+// Last updated: 7/14/2026, 3:25:08 PM
+class Solution {
+    public boolean isCovered(int[][] ranges, int left, int right) {
+        boolean flag = false;
+        for (int i=left; i<=right; i++) {
+            for (int[] arr: ranges) {
+                if (i >= arr[0] && i <= arr[1]) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) return false;
+            flag = false;
+        }
+        
+        return true;
+    }
+}
