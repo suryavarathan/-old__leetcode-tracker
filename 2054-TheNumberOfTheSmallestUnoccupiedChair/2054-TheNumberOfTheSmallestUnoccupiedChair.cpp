bc0d3a6f75414894dea4cdd5773cd6f54f69d0a4
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:23:58 +0530

    Create 2054-TheNumberOfTheSmallestUnoccupiedChair

---
+++ 2054-TheNumberOfTheSmallestUnoccupiedChair/2054-TheNumberOfTheSmallestUnoccupiedChair.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/14/2026, 3:23:58 PM
+class Solution {
+public:
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        int n = times.size();
+        vector<int> chairs(n, -1);
+        int tar = times[targetFriend][0];
+        sort(times.begin(), times.end());
+        for (auto it : times) {
+            int arr = it[0];
+            int dep = it[1];
+            for (int i = 0; i < n; i++) {
+                if (arr >= chairs[i]) {
+                    chairs[i] = dep;
+                    if (arr == tar)
+                        return i;
+                    break;
+                }
+            }
+        }
+        return -1;
+    }
+};
