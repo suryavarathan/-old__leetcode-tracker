d97bb40299fc3f5da42fab71cbd9146fbf921b67
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:24:27 +0530

    Create 2034-MinimumAbsoluteDifferenceQueries

---
+++ 2034-MinimumAbsoluteDifferenceQueries/2034-MinimumAbsoluteDifferenceQueries.py
@@ -0,0 +1,17 @@
+# Last updated: 7/14/2026, 3:24:27 PM
+class Solution:
+    def minDifference(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        loc = {}
+        for i, x in enumerate(nums): loc.setdefault(x, []).append(i)
+        keys = sorted(loc)
+        
+        ans = []
+        for l, r in queries: 
+            prev, val = 0, inf
+            for x in keys: 
+                i = bisect_left(loc[x], l)
+                if i < len(loc[x]) and loc[x][i] <= r: 
+                    if prev: val = min(val, x - prev)
+                    prev = x 
+            ans.append(val if val < inf else -1)
+        return ans
