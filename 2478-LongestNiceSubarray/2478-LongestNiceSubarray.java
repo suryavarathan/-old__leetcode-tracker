c9761dcd26f13edf03c816d96703d9bc5d4d17f5
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:22:02 +0530

    Create 2478-LongestNiceSubarray

---
+++ 2478-LongestNiceSubarray/2478-LongestNiceSubarray.java
@@ -0,0 +1,22 @@
+// Last updated: 7/14/2026, 3:22:02 PM
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int maxLength = 0;
+        int orValue = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            
+            while ((orValue & nums[right]) != 0) {
+                orValue ^= nums[left];
+                left++;
+            }
+
+            orValue |= nums[right];
+            
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
