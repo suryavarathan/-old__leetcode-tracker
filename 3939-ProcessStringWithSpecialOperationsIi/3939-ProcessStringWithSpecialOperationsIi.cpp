516b2639a850cf0bdd8c34eec6de662eb980eab8
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:20:03 +0530

    Create 3939-ProcessStringWithSpecialOperationsIi

---
+++ 3939-ProcessStringWithSpecialOperationsIi/3939-ProcessStringWithSpecialOperationsIi.cpp
@@ -0,0 +1,40 @@
+// Last updated: 7/14/2026, 3:20:03 PM
+class Solution {
+    typedef long long ll;
+public:
+    char processStr(string s, ll k) {
+        int n = s.size();
+        vector<ll> lens;
+        ll len = 0;
+
+        for (auto& ch : s) {
+            if (ch == '*')
+                len = max(len - 1, 0LL);
+            else if (ch == '#')
+                len *= 2;
+            else if (ch != '%')
+                len++;
+
+            lens.push_back(len);
+        }
+
+        if (k >= len) return '.';
+
+        for (int i = n - 1;; i--) {
+            switch (s[i]) {
+            case '*':
+                break;
+            case '#':
+                if (k >= lens[i] / 2)
+                    k -= lens[i] / 2;
+                break;
+            case '%':
+                k = lens[i] - 1 - k;
+                break;
+            default: // s[i] is a character
+                if (lens[i] == k + 1)
+                    return s[i];
+            }
+        }
+    }
+};
