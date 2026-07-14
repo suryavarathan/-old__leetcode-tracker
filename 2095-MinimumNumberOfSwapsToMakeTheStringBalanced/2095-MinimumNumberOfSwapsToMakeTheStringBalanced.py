40fcd7b82bdd2fe654e01c287c2b71a81623b7af
Surya V <suryavarathan46@gmail.com>
2026-07-14 15:23:07 +0530

    Create 2095-MinimumNumberOfSwapsToMakeTheStringBalanced

---
+++ 2095-MinimumNumberOfSwapsToMakeTheStringBalanced/2095-MinimumNumberOfSwapsToMakeTheStringBalanced.py
@@ -0,0 +1,20 @@
+# Last updated: 7/14/2026, 3:23:07 PM
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        balance = 0
+        max_imbalance = 0
+        
+        # Traverse the string
+        for char in s:
+            if char == '[':
+                balance += 1  # Opening bracket, increase balance
+            else:
+                balance -= 1  # Closing bracket, decrease balance
+            
+            # Track the maximum imbalance (i.e., the maximum negative balance)
+            if balance < 0:
+                max_imbalance = max(max_imbalance, -balance)
+        
+        # The number of swaps needed is half of the max imbalance
+        # because each swap fixes two imbalances (one opening, one closing).
+        return (max_imbalance + 1) // 2
